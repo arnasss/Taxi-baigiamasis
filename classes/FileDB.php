af382a0cc84b3d65bc599d2e5f478317a99b6f1d
arnasss <arnasile@gmail.com>
2019-10-22 11:54:21 +0300

    fileDB patvarkitas

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -4,7 +4,7 @@ class FileDB {
 
     private $file_name;
 
-    private function _construct($file_name) {
+    public function __construct($file_name) {
         $this->file_name = $file_name;
     }
 }
