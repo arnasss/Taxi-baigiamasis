1152a4cfd99c8e4a9072850d2b5e58fccef13d3d
arnasss <arnasile@gmail.com>
2019-10-22 12:27:15 +0300

    gngf

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,10 +1,18 @@
 <?php
 
+require 'functions/file.php';
+
 class FileDB {
 
     private $file_name;
+    private $data;
 
     public function __construct($file_name) {
         $this->file_name = $file_name;
+    }
+    
+    public function load() {
+        $array = file_to_array($this -> file_name);
+        return $array;
     }
 }
