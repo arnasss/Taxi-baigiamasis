f538b2fe136548d65fbe04fc00881ba87d90dcc1
arnasss <arnasile@gmail.com>
2019-10-23 09:23:36 +0300

    gngf

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -5,8 +5,9 @@ require 'functions/file.php';
 class FileDB {
 
     private $file_name;
-    /** @var array Duomenu masyvas */
     private $data;
+    private $table_name;
+    
 
     public function __construct($file_name) {
         $this->file_name = $file_name;
@@ -21,6 +22,29 @@ class FileDB {
     }
     
     public function save() {
-        array_to_file($this->data, $this->file_name);
+        return array_to_file($this->data, $this->file_name);
+    }
+    
+    public function getRow($table, $row_id) {
+        if (isset($this->data[$table])) {
+            return $this->data[$table][$row_id];
+        }
+    }
+    
+    public function addRow($table, $row) {
+        $this->data[$table][] = $row;
+    }
+    
+    public function replaceRow($table, $row, $row_id) {
+        $this->data[$table][$row_id] = $this->row;
+    }
+    
+    public function createTable($table_name) {
+        if (!isset($table_name)) {
+            $this->table_name = $table_name[];
+        }
     }
 }
+
+
+
